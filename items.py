ed6fb7f45c81a653529a949a267cb046f21ba85a
SpaceSocks <ryan.geidl@gmail.com>
2024-08-14 13:01:56 -0500

    Create file items.py

---
+++ items.py
@@ -0,0 +1,7 @@
+class Item:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+# Example usage:
+# sword = Item('Sword', 'A sharp blade.')
